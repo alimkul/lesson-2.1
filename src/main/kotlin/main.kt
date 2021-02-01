4036d4441ec35ed4bd3a3a7436bac9331ad64612
Archie <alimkul.shynggys@gmail.com>
2021-02-01 20:55:27 +0600

    Update2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main(){
     var amount :Int =5000
     var comissia = amount*0.75/100
-    if(!(comissia >=35))
+    if(comissia <=35)
         comissia=35.00;
     println("Amount: "+(amount - comissia)+ " commissia: "+comissia)
 }
